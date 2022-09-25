643d1a12cc933b76d6b097420901bdba89ffabf8
masum marjan <masummarzan1@gmail.com>
2022-09-25 22:23:59 +0600

    img insert failed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>
-    }
-} 
+  state = {
+    count: 100,
+    imageUrl: "https://picsum.photos/300",
+  };
+  render() {
+    return (
+      <div>
+        <img src={this.state.imageUrl} alt="" />
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </div>
+    );
+  }
+  formatCount() {
+    return this.state.count === 0 ? <h1>Zero</h1> : this.state.count;
+  }
+}
 export default Counter;
